7e1efed0788ac084146f5539ec0380d02e134f2f
Chanumolu Sree Varsha <90399281+VarshaChanumolu@users.noreply.github.com>
2025-04-16 13:12:22 -0400

    Update _bld_met_mtx.py

--- episcanpy/count_matrix/_bld_met_mtx.py
+++ episcanpy/count_matrix/_bld_met_mtx.py
@@ -1,5 +1,5 @@
 import numpy as np
-from numpy inport Nan
+from numpy inport NaN
 
 # chromosomes for 2 principal species. If you work with another genome
 # the chromosomes will have to be specified
